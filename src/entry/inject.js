8f59fdc6526d0c0461fe3a13ed8c9fcf389ece87
bafang <ningfangxiao@futurevista.work>
2023-06-29 10:13:59 +0800

    :bug: 修复拦截metamask和okxwallet重复的bug

--- src/entry/inject.js
+++ src/entry/inject.js
@@ -67,150 +67,147 @@ function continueRequest(metamaskRequest, ethereumRequestArguments, resolve, rej
         .catch((error) => reject(error));
 }
 
-// proxy request
-if (window.ethereum && window.ethereum.request) {
-    const metamaskRequest = window.ethereum.request;
-    const customRequest = ({ ...ethereumRequestArguments }) => {
-        return new Promise((resolve, reject) => {
-            // console.log('sdfa', ethereumRequestArguments)
-            if (!supportNetwork.includes(sessionStorage.getItem('network'))) {
-                continueRequest(metamaskRequest, ethereumRequestArguments, resolve, reject, 0)
-                return;
-            }
-
-            if (method.includes(ethereumRequestArguments.method)) {
-                // console.log('拦截到了',ethereumRequestArguments)
-                postEvent(metamaskRequest, ethereumRequestArguments, resolve, reject).then(() => {})
-            } else if (ethereumRequestArguments.method === 'eth_chainId') {
-                // type为 1 获取chainId，0待定
-                continueRequest(metamaskRequest, ethereumRequestArguments, resolve, reject, 1)
-            } else {
-                continueRequest(metamaskRequest, ethereumRequestArguments, resolve, reject, 0)
-            }
-        });
-    };
-
-    window.ethereum.request = customRequest;
-}
-
-// proxy sendAsync
-const proxy1 = new Proxy(window.ethereum.sendAsync, {
-    apply(target, thisArg, argArray) {
-        const [request, callback] = argArray;
-        if (!request) {
-            return Reflect.apply(target, thisArg, argArray);
-        }
-
-        if (!supportNetwork.includes(sessionStorage.getItem('network'))) {
-            return Reflect.apply(target, thisArg, argArray);
-        }
-
-        if (method.includes(request.method)) {
-            // console.log('sendAsync 代理成功')
-            let uuid = crypto.randomUUID()
-            const chainId = sessionStorage.getItem('network')
-            let params
-
-            RecognizeTransaction(chainId, request, uuid).then(res => {
-                if (!res) {
-                    return
-                }
-                params = res
-                let event = new CustomEvent('ByteHunter-Message', {
-                    detail: {
-                        uuid: uuid,
-                        params: res,
-                        type: 1
-                    }
-                })
-                window.dispatchEvent(event)
-            })
-
-            let event = new CustomEvent('ByteHunter-Message', {detail: {uuid: uuid, type: 0, params: params}})
-            window.dispatchEvent(event)
-            window.addEventListener(uuid, (event) => {
-                if (event.detail.confirm) {
-                    request.params.fromExtension = true;
-                    // console.log(argArray)
-                    return Reflect.apply(target, thisArg, argArray)
-                } else if (event.detail.cancel) {
-                    const error = ethErrors.provider.userRejectedRequest(
-                        'User denied message signature.'
-                    );
-                    const response = {
-                        id: request?.id,
-                        jsonrpc: '2.0',
-                        error,
-                    };
-                    callback(error, response);
-                }
-            }, {once: true})
-        } else {
-            return Reflect.apply(target, thisArg, argArray);
-        }
-    }
-})
-window.ethereum.sendAsync = proxy1
-
-// proxy send
-const proxy = new Proxy(window.ethereum.send, {
-    apply(target, thisArg, argArray) {
-        if (!supportNetwork.includes(sessionStorage.getItem('network'))) {
-            return Reflect.apply(target, thisArg, argArray);
-        }
-        // console.log('代理send成功')
-
-        const [payloadOrMethod, callbackOrParams] = argArray;
-
-        // ethereum.send has three overloads:
-
-        // ethereum.send(method: string, params?: Array<unknown>): Promise<JsonRpcResponse>;
-        // > gets handled like ethereum.request
-        if (typeof payloadOrMethod === 'string') {
-            return window.ethereum.request({ method: payloadOrMethod, params: callbackOrParams });
-        }
-
-        // ethereum.send(payload: JsonRpcRequest): unknown;
-        // > cannot contain signature requests
-        if (!callbackOrParams) {
-            return Reflect.apply(target, thisArg, argArray);
-        }
-
-        // ethereum.send(payload: JsonRpcRequest, callback: JsonRpcCallback): void;
-        // > gets handled like ethereum.sendAsync
-        return window.ethereum.sendAsync(payloadOrMethod, callbackOrParams);
-    }
-})
-window.ethereum.send = proxy
-
-if (window.okxwallet && window.okxwallet.request) {
-    const metamaskRequest = window.okxwallet.request;
-    const customRequest = ({ ...ethereumRequestArguments }) => {
-        return new Promise((resolve, reject) => {
-            // console.log('sdfa', ethereumRequestArguments)
-            if (!supportNetwork.includes(sessionStorage.getItem('network'))) {
-                continueRequest(metamaskRequest, ethereumRequestArguments, resolve, reject, 0)
-                return;
-            }
-
-            if (method.includes(ethereumRequestArguments.method)) {
-                // console.log('拦截到了',ethereumRequestArguments)
-                postEvent(metamaskRequest, ethereumRequestArguments, resolve, reject).then(() => {})
-            } else if (ethereumRequestArguments.method === 'eth_chainId') {
-                // type为 1 获取chainId，0待定
-                continueRequest(metamaskRequest, ethereumRequestArguments, resolve, reject, 1)
-            } else {
-                continueRequest(metamaskRequest, ethereumRequestArguments, resolve, reject, 0)
-            }
-        });
-    };
-
-    window.okxwallet.request = customRequest;
-}
-
-// proxy sendAsync
 if (window.okxwallet) {
+    if (window.okxwallet.request) {
+        const metamaskRequest = window.okxwallet.request;
+        const customRequest = ({ ...ethereumRequestArguments }) => {
+            return new Promise((resolve, reject) => {
+                // console.log('sdfa', ethereumRequestArguments)
+                if (!supportNetwork.includes(sessionStorage.getItem('network'))) {
+                    continueRequest(metamaskRequest, ethereumRequestArguments, resolve, reject, 0)
+                    return;
+                }
+
+                if (method.includes(ethereumRequestArguments.method)) {
+                    // console.log('拦截到了',ethereumRequestArguments)
+                    postEvent(metamaskRequest, ethereumRequestArguments, resolve, reject).then(() => {})
+                } else if (ethereumRequestArguments.method === 'eth_chainId') {
+                    // type为 1 获取chainId，0待定
+                    continueRequest(metamaskRequest, ethereumRequestArguments, resolve, reject, 1)
+                } else {
+                    continueRequest(metamaskRequest, ethereumRequestArguments, resolve, reject, 0)
+                }
+            });
+        };
+
+        window.okxwallet.request = customRequest;
+    }
+    // proxy sendAsync
     window.okxwallet.sendAsync = new Proxy(window.okxwallet.sendAsync, {
+            apply(target, thisArg, argArray) {
+                const [request, callback] = argArray;
+                if (!request) {
+                    return Reflect.apply(target, thisArg, argArray);
+                }
+
+                if (!supportNetwork.includes(sessionStorage.getItem('network'))) {
+                    return Reflect.apply(target, thisArg, argArray);
+                }
+
+                if (method.includes(request.method)) {
+                    // console.log('sendAsync 代理成功')
+                    let uuid = crypto.randomUUID()
+                    const chainId = sessionStorage.getItem('network')
+                    let params
+
+                    RecognizeTransaction(chainId, request, uuid).then(res => {
+                        if (!res) {
+                            return
+                        }
+                        params = res
+                        let event = new CustomEvent('ByteHunter-Message', {
+                            detail: {
+                                uuid: uuid,
+                                params: res,
+                                type: 1
+                            }
+                        })
+                        window.dispatchEvent(event)
+                    })
+
+                    let event = new CustomEvent('ByteHunter-Message', {detail: {uuid: uuid, type: 0, params: params}})
+                    window.dispatchEvent(event)
+                    window.addEventListener(uuid, (event) => {
+                        if (event.detail.confirm) {
+                            request.params.fromExtension = true;
+                            // console.log(argArray)
+                            return Reflect.apply(target, thisArg, argArray)
+                        } else if (event.detail.cancel) {
+                            const error = ethErrors.provider.userRejectedRequest(
+                                'User denied message signature.'
+                            );
+                            const response = {
+                                id: request?.id,
+                                jsonrpc: '2.0',
+                                error,
+                            };
+                            callback(error, response);
+                        }
+                    }, {once: true})
+                } else {
+                    return Reflect.apply(target, thisArg, argArray);
+                }
+            }
+        })
+
+    // proxy send
+    window.okxwallet.send = new Proxy(window.okxwallet.send, {
+            apply(target, thisArg, argArray) {
+                if (!supportNetwork.includes(sessionStorage.getItem('network'))) {
+                    return Reflect.apply(target, thisArg, argArray);
+                }
+                // console.log('代理send成功')
+
+                const [payloadOrMethod, callbackOrParams] = argArray;
+
+                // ethereum.send has three overloads:
+
+                // ethereum.send(method: string, params?: Array<unknown>): Promise<JsonRpcResponse>;
+                // > gets handled like ethereum.request
+                if (typeof payloadOrMethod === 'string') {
+                    return window.ethereum.request({method: payloadOrMethod, params: callbackOrParams});
+                }
+
+                // ethereum.send(payload: JsonRpcRequest): unknown;
+                // > cannot contain signature requests
+                if (!callbackOrParams) {
+                    return Reflect.apply(target, thisArg, argArray);
+                }
+
+                // ethereum.send(payload: JsonRpcRequest, callback: JsonRpcCallback): void;
+                // > gets handled like ethereum.sendAsync
+                return window.ethereum.sendAsync(payloadOrMethod, callbackOrParams);
+            }
+        })
+} else {
+    // proxy request
+    if (window.ethereum && window.ethereum.request) {
+        const metamaskRequest = window.ethereum.request;
+        const customRequest = ({ ...ethereumRequestArguments }) => {
+            return new Promise((resolve, reject) => {
+                // console.log('sdfa', ethereumRequestArguments)
+                if (!supportNetwork.includes(sessionStorage.getItem('network'))) {
+                    continueRequest(metamaskRequest, ethereumRequestArguments, resolve, reject, 0)
+                    return;
+                }
+
+                if (method.includes(ethereumRequestArguments.method)) {
+                    console.log('拦截到了',ethereumRequestArguments)
+                    postEvent(metamaskRequest, ethereumRequestArguments, resolve, reject).then(() => {})
+                } else if (ethereumRequestArguments.method === 'eth_chainId') {
+                    // type为 1 获取chainId，0待定
+                    continueRequest(metamaskRequest, ethereumRequestArguments, resolve, reject, 1)
+                } else {
+                    continueRequest(metamaskRequest, ethereumRequestArguments, resolve, reject, 0)
+                }
+            });
+        };
+
+        window.ethereum.request = customRequest;
+    }
+
+    // proxy sendAsync
+    const proxy1 = new Proxy(window.ethereum.sendAsync, {
         apply(target, thisArg, argArray) {
             const [request, callback] = argArray;
             if (!request) {
@@ -266,11 +263,10 @@ if (window.okxwallet) {
             }
         }
     })
-}
+    window.ethereum.sendAsync = proxy1
 
-// proxy send
-if (window.okxwallet) {
-    window.okxwallet.send = new Proxy(window.okxwallet.send, {
+    // proxy send
+    const proxy = new Proxy(window.ethereum.send, {
         apply(target, thisArg, argArray) {
             if (!supportNetwork.includes(sessionStorage.getItem('network'))) {
                 return Reflect.apply(target, thisArg, argArray);
@@ -284,7 +280,7 @@ if (window.okxwallet) {
             // ethereum.send(method: string, params?: Array<unknown>): Promise<JsonRpcResponse>;
             // > gets handled like ethereum.request
             if (typeof payloadOrMethod === 'string') {
-                return window.ethereum.request({method: payloadOrMethod, params: callbackOrParams});
+                return window.ethereum.request({ method: payloadOrMethod, params: callbackOrParams });
             }
 
             // ethereum.send(payload: JsonRpcRequest): unknown;
@@ -298,6 +294,7 @@ if (window.okxwallet) {
             return window.ethereum.sendAsync(payloadOrMethod, callbackOrParams);
         }
     })
+    window.ethereum.send = proxy
 }
 
 // proxy postMessage
